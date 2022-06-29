3d24cf3058fd7f084c161fffbc815a78ae374796
leepython <leepython.forever@gmail.com>
2022-06-29 12:31:03 +0300

    a first tree code

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-# This is a sample Python script.
+# This is a sample tree
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
+n = int(input())
+for i in range(1,n+1):
+    for n in range(1,i+1):
+        print (n)
