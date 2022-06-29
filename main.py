349a370f1f3b84ce246390bbf23980642b44f8fc
leepython <leepython.forever@gmail.com>
2022-06-29 12:43:58 +0300

    a simple tree

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-# This is a sample tree
+# This is a simple tree
 
 n = int(input())
 for i in range(1,n+1):
     for n in range(1,i+1):
-        print (n)
+        print (n,sep='', end='')
+    print()
