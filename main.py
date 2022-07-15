03a90237900a423053ba43413f8923a439966cb2
leepython <leepython.forever@gmail.com>
2022-07-15 23:05:14 +0300

    Analog of admin base v.001

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
-# This is a simple calculator
-print('*' * 15, 'Calculator', '*' * 15)
-print('Press q to exit')
-while True:
-    sign = input('Enter a sign: +, -, *, /')
-    if sign == 'q':
-        break
-    if sign in ('+','-','*','/'):
-        x = float(input('Enter a value of x: '))
-        y = float(input('Enter a value of y: '))
-        if sign == '+':
-            print('Answer is: ', x+y)
-        elif sign == '-':
-            print('Answer is: ', x-y)
-        elif sign == '*':
-            print('Answer is: ', x*y)
-        elif sign == '/':
-            if y != 0:
-                print('Answer is: ', x / y)
-            else:
-                print('divide-by-zero error')
-    else:
-        print('Error: invalid sign of operation')
+#Analog of admin base
+level = 0       #Стандарнтый уровень доступа
+login = ''
+password = ''
+while not login:
+    login = input('Введите имя Вашей учетной записи: ')
+while not password:
+    password = input('Введите пароль от Вашей учтеной записи: ')
+
+if login == 'admin' and password == 'root':
+    level = 100
+elif login == 'omnomnom' and password == 'baton':
+    level = 99
+
+if level:
+    print('Добро пожаловать в игру,', login)
+    print('Ваш ранг в игре: ', level)
+else:
+    print('Логин или пароль введены неверно, \n Доступ заблокирован!')
