55687f795c18a7fb53642ab2d0ab98079b38a016
master <leepython.forever@gmail.com>
2022-07-21 01:10:24 +0300

    rr complete

--- mini_proramms/russian_roulette.py
+++ mini_proramms/russian_roulette.py
@@ -1,14 +1,14 @@
 #   Привет, здесь я попробую создать рандомайзер с игровой механикой
 
 import random
-print('*' * 15, 'let\'s play russian roulette','*' * 15)
+print('\n\n','*' * 15, 'let\'s play Russian Roulette','*' * 15)
 print('The rules are simple. Stay alive as long as possible and take a place in the ranking!')
 
 score = 0
 death = 0
 
 while True:
-    answer = str(input('Enter a number between 1 and 6. Press q to exit \n\n\n'))
+    answer = str(input('Enter a number between 1 and 6. Press q to exit \n\n'))
     rand = str(random.randint(1, 6))  #  Генерирует случайное число в диапазоне (x,y)
     if answer == 'q':   #   Выход из цикла
         break
@@ -19,8 +19,3 @@ while True:
         death +=1   #   Счетчик смертей
         print(f'You died {death} times')
 print(f'Your score is {score}. But you died {death} times. See you next time!')
-
-
-
-
-
