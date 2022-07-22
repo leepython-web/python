ab4bb1731b3a2162caaa45ef2252acd311b4e7af
master <leepython.forever@gmail.com>
2022-07-23 01:48:11 +0300

    commented slices

--- mini_proramms/slice.py
+++ mini_proramms/slice.py
@@ -1,13 +1,16 @@
 # В решении этой задачи практикуюсь на срезах. Суть задачи: есть два друга гооворящие на вымышленной языке - Pig Latin
 # в котором первая буква слова переносится в конец, далее добавляется 'ay'. road = oadray
-# Задача: дана фраза на английском, которую надо перевести на Pig Latin
+# Задача: дана фраза произвольной длины на английском, которую надо перевести на Pig Latin, к примеру:
 # Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium
-phrase = input()
-lst = phrase.split()
-n_word = None
-new_phrase = ''
 
-for word in lst:
-    n_word = word[1:] + word[0] + 'ay '
+phrase = input()
+
+lst = phrase.split()    #разделение строки на подстроки(слова). По умлочанию, в качестве разделителя выбран пробел
+
+n_word = None   # Слово на Pig Latin
+new_phrase = '' # Новая фраза с учетом изменений
+
+for word in lst:    # Цикл перебирающий каждый фрагмент а lst
+    n_word = word[1:] + word[0] + 'ay ' # Генерация нового слова
     new_phrase = new_phrase + n_word
 print(new_phrase)
