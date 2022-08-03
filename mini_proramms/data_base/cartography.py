76deb9df2e37d1a42f8b0885cafb65faf025da27
leepython <leepython.forever@gmail.com>
2022-08-03 14:53:32 +0300

    update

--- mini_proramms/data_base/cartography.py
+++ mini_proramms/data_base/cartography.py
@@ -2,10 +2,16 @@
 import re
 
 data = input('Введите ваше ФИО и дату рождения в формате ДД/ММ/ГГ \n')
-data = re.split(r'[,;./\s]\s*', data)     # re.split добавляет большей гибкости к разделителям
-surname, name, patronymic, *date = data
+new_data = re.split(r'[,;./\s]\s*', data)     # re.split добавляет большей гибкости к разделителям
+surname, name, patronymic, *date = new_data
 
 print(name, date)
 
 datepat = re.compile(r'(\d+)/(\d+)/(\d+)')    # Создание маски для поиска с группами захвата
 date = datepat.findall(data)
+print('All dates: ', date)
+
+def change_date():
+    refactor = re.sub(r'(\d+)/(\d+)/(\d+)', r'\3-\1-\2', data)
+    print('Another format of date: ', refactor)
+change_date()
