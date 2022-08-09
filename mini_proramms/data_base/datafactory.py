2158253fbf9f619ee0318f094924854e3023ace5
leepython <leepython.forever@gmail.com>
2022-08-09 23:40:55 +0300

    remade

--- mini_proramms/data_base/datafactory.py
+++ mini_proramms/data_base/datafactory.py
@@ -1,6 +1,12 @@
 
-txt = open('data.txt', 'r', encoding='utf-8')
-content = txt.read()
-print(content)
-txt.close()
+def user_name():
+    name = None
+    count = 0
+    txt = open('data.txt', 'w', encoding='utf-8')
 
+    while name != 'q':
+        count += 1
+        name = input('Enter your name: ')
+        txt.write(f'\n {count}) {name}')
+    txt.close()
+user_name()
