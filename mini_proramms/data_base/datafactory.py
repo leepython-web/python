8a8f6b97e60f62cbf1303078d5d98b3568bf207c
master <leepython.forever@gmail.com>
2022-08-28 16:05:11 +0300

    update

--- mini_proramms/data_base/datafactory.py
+++ mini_proramms/data_base/datafactory.py
@@ -1,20 +1,6 @@
-print('Для выхода из программы введите q')
-def user_name():
-    name = None
-    count = 0
-    txt = open('data.txt', 'w', encoding='utf-8')
 
-    while name != 'q':
-        count += 1
-        name = input('Enter your name: ')
-        txt.write(f'\n {count}) {name}')
-    txt.close()
+txt = open('data.txt', 'r', encoding='utf-8')
+content = txt.read()
+print(content)
+txt.close()
 
-def read_user_name():
-    txt = open('data.txt', 'r', encoding='utf-8')
-    for line in txt:
-        print(line)
-    txt.close()
-
-user_name()
-read_user_name()
---
+++ mini_proramms/data_base/mysqltest.py
@@ -0,0 +1,17 @@
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='0000',
+    database='userscore'
+)
+
+my_cursor = mydb.cursor()
+sql_formula = 'INSERT INTO userscoretab (username, score) VALUES (%s, %s)'
+user1 = ('Максим', 12)
+
+my_cursor.execute(sql_formula, user1)
+
+mydb.commit()
