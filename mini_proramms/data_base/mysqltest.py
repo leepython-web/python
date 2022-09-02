9542aa575c2781d9e3132972e307d6828e578f02
leepython <leepython.forever@gmail.com>
2022-09-02 15:11:31 +0300

    update

--- mini_proramms/data_base/mysqltest.py
+++ mini_proramms/data_base/mysqltest.py
@@ -1,17 +1,18 @@
 
 import mysql.connector
 
-mydb = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    passwd='0000',
-    database='userscore'
-)
+def rating():
+    mydb = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        passwd='0000',
+        database='userscore'
+    )
 
-my_cursor = mydb.cursor()
-sql_formula = 'INSERT INTO userscoretab (username, score) VALUES (%s, %s)'
-user1 = ('Максим', 12)
+    my_cursor = mydb.cursor()
+    sql_formula = 'INSERT INTO userscoretab (username, score) VALUES (%s, %s)'
+    user1 = ('Максим', 12)
 
-my_cursor.execute(sql_formula, user1)
+    my_cursor.execute(sql_formula, user1)
 
-mydb.commit()
+    mydb.commit()
