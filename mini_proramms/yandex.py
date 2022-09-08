a0d74041225f38bdf960bae4ce7df33292cbc096
leepython <leepython.forever@gmail.com>
2022-09-08 12:27:57 +0300

    update

--- mini_proramms/yandex.py
+++ mini_proramms/yandex.py
@@ -1,3 +1,9 @@
-a = input()
+inputFile = open('input.txt', 'r', encoding='utf-8')
+a = inputFile.read()
 nums = a.split()
-print(int(nums[0]) + int(nums[1]))
+answer = int(nums[0]) + int(nums[1])
+inputFile.close
+
+outputFile = open('output.txt', 'w', encoding='utf-8')
+outputFile.write(answer)
+outputFile.close
