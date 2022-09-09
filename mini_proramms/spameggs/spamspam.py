56b0132290e01a0102c10137d5d68ecc1bc0100b
leepython <leepython.forever@gmail.com>
2022-09-10 00:19:16 +0300

    fix a sendMessage

--- mini_proramms/spameggs/spamspam.py
+++ mini_proramms/spameggs/spamspam.py
@@ -3,7 +3,7 @@ import time, pyautogui
 
 def sendMessage():
     time.sleep(2)
-    message = 'EGGS EGGS EGGS'
+    message = 'EGGS'
     iterations = 10
 
     for i in range(iterations):
@@ -12,17 +12,17 @@ def sendMessage():
     while iterations > 0:
         iterations -= 1
 
-    pyautogui.typewrite(message.strip())
-    pyautogui.press('enter')
+        pyautogui.typewrite(message)
+        pyautogui.press('enter')
 
-print('Получатель получил свинцовую пулю')
+    print('Получатель получил свинцовую пулю')
 
 def SendText():
     time.sleep(3)
     with open('eggs.txt') as f: # Открывает блокнот с названием text.txt (документ с содержанием того, что мы хотим отправлять).
         lines = f.readline()
     for line in lines:
-        pyautogui.typewrite(line.strip()) # В этой функции оно будет писать текст с каждой строки
+        pyautogui.typewrite(line) # В этой функции оно будет писать текст с каждой строки
         pyautogui.typewrite('enter') # и так же отправлять его "жертве".
     print("Дело сделано, осталось успокоить нашу жертву ^_^")
 
