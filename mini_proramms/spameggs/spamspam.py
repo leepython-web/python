e0a983e2724db358201f44892cbf989a0f385813
leepython <leepython.forever@gmail.com>
2022-09-11 14:01:57 +0300

    spamspam.py complete

--- mini_proramms/spameggs/spamspam.py
+++ mini_proramms/spameggs/spamspam.py
@@ -32,25 +32,12 @@ def sendMessage():
     print('Получатель получил свинцовую пулю')
 
 def SendText():
-    time.sleep(3)
-    with open('eggs.txt') as f: # Открывает блокнот с названием text.txt (документ с содержанием того, что мы хотим отправлять).
-        lines = f.readline()
+    time.sleep(5)
+    with open('egg.txt') as f: # Открывает блокнот с названием text.txt (документ с содержанием того, что мы хотим отправлять).
+        lines = f.readlines()
     for line in lines:
         pyautogui.typewrite(line) # В этой функции оно будет писать текст с каждой строки
         pyautogui.typewrite('enter') # и так же отправлять его "жертве".
     print("Дело сделано, осталось успокоить нашу жертву ^_^")
-
-print('~'*50)
-print("[1] ===> Стрелять одним сообщением указанным в переменной ")
-print("[2] ===> Отправлять строки из блокнота ")
-print('~'*50)
-option = input("[Выбирай функцию]===> ")
-
-if option == "1":
-    sendMessage()
-elif option == "2":
-    SendText()
-else:
-    print('Выбирай функция 1 или 2!')
 
 mainMenu()
--- mini_proramms/spameggs/yandex1.py
+++
@@ -1,4 +0,0 @@
-file = open('eggs.txt', 'r', encoding='utf-8')
-content = file.readlines()
-for line in content:
-    print(line)
