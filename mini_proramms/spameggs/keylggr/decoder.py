9461b21559689f2d3371dd34987f0d83c7e2caf5
leepython <leepython.forever@gmail.com>
2022-11-05 00:36:04 +0300

    decoder.py remake and adding some defs

--- mini_proramms/spameggs/keylggr/decoder.py
+++ mini_proramms/spameggs/keylggr/decoder.py
@@ -2,11 +2,35 @@ import rsa
 
 pubkey, privkey = rsa.newkeys(512)
 
-str1 = ('HELLO MY DEAR FRIEND')
+message = ('HELLO MY DEAR FRIEND')
 
-enctex = rsa.encrypt(str1.encode(), pubkey)
+
 
 dectex = rsa.decrypt(enctex, privkey).decode()
-print("The primordial string: ", str1)
-print("The Encrypted message: ", enctex)
+print("The primordial string: ", message)
+
 print("The Decrypted message: ", dectex)
+
+def mainMenu():
+    print('''Message senler v0.001. @All rights reserved
+    1. Send message
+    2. Decode message
+    3. Keys''')
+    menuInput = int(input())
+    if menuInput == 1:
+        print('Send message')
+        sendMessage
+    elif menuInput == 2:
+        print('Decode message')
+        decodeMessage
+    elif menuInput == 3:
+        print('Keys')
+        keys
+    else:
+        print('Invalid input, please try again')
+
+def sendMessage():
+    print('Enter your message: ')
+    message = str(input())
+    enctex = rsa.encrypt(message.encode(), pubkey)
+    print("The Encrypted message: ", enctex)
