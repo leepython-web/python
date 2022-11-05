96e0529848800fbe54894ffc048e689b53d12b23
leepython <leepython.forever@gmail.com>
2022-11-06 00:26:22 +0300

    decoder.py remake and upgrade mainMenu

--- mini_proramms/spameggs/keylggr/decoder.py
+++ mini_proramms/spameggs/keylggr/decoder.py
@@ -1,36 +1,42 @@
 import rsa
-
 pubkey, privkey = rsa.newkeys(512)
-
-message = ('HELLO MY DEAR FRIEND')
-
-
-
-dectex = rsa.decrypt(enctex, privkey).decode()
-print("The primordial string: ", message)
-
-print("The Decrypted message: ", dectex)
 
 def mainMenu():
     print('''Message senler v0.001. @All rights reserved
     1. Send message
     2. Decode message
     3. Keys''')
-    menuInput = int(input())
-    if menuInput == 1:
+    menuInput = input()
+    if menuInput == '1':
         print('Send message')
-        sendMessage
-    elif menuInput == 2:
+        sendMessage()
+    elif menuInput == '2':
         print('Decode message')
-        decodeMessage
-    elif menuInput == 3:
+        decodeMessage()
+    elif menuInput == '3':
         print('Keys')
-        keys
+        keys()
     else:
         print('Invalid input, please try again')
 
 def sendMessage():
+    global enctex
     print('Enter your message: ')
     message = str(input())
     enctex = rsa.encrypt(message.encode(), pubkey)
     print("The Encrypted message: ", enctex)
+    mainMenu()
+
+def decodeMessage():
+    print('Enter your encrypted message: ')
+    global enctex
+    dectex = rsa.decrypt(enctex, privkey).decode()
+    print("The Decrypted message: ", dectex)
+    mainMenu()
+def keys():
+    print(f'''Your encryption keys: 
+    {pubkey}
+    {privkey}''')
+    mainMenu()
+
+mainMenu()
