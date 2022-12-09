89ea02d890fc210bb6397dd3aa8abffb09d02ab6
leepython <leepython.forever@gmail.com>
2022-12-10 02:38:32 +0300

    create new page - heroes.html, superuser, method __str__ in models.py  (for convenience in the admin panel) add some strings in database, and other changes

--- web/djangoreferences/frontapp/views.py
+++ web/djangoreferences/frontapp/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def index_page(request):
-    return  render(request, 'index.html')
+    return render(request, 'index.html')
+
+def heroes(request):
+    return render(request, 'heroes.html')
