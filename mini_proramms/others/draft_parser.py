bb00eea4ccb64e9178d868096603e9792e5589bb
leepython <leepython.forever@gmail.com>
2023-05-23 13:30:09 +0300

    reorganized draft_parser.py

--- mini_proramms/others/draft_parser.py
+++ mini_proramms/others/draft_parser.py
@@ -2,14 +2,15 @@ import requests
 from bs4 import BeautifulSoup as bs
 import pandas as pd
 
-result = []
-
 URL_TEMPLATE = 'https://wildrift-wiki.com/assassins/283-riven.html'
-r = requests.get(URL_TEMPLATE)
-soup = bs(r.text, 'html.parser')
-characteristics = soup.find_all('table', class_='fr-solid-borders fr-alternate-rows')
-for parametr in characteristics:
-    result.append(f' {parametr.text}, ')
 
-print(result)
+def draft_parser(URL_TEMPALTE):
+    result = []
+    r = requests.get(URL_TEMPLATE)
+    soup = bs(r.text, 'html.parser')
+    characteristics = soup.find('table', class_='fr-solid-borders fr-alternate-rows').find_all('td')
+    for parametr in characteristics:
+        result.append(parametr.text)
+    print(result)
 
+draft_parser(URL_TEMPLATE)
